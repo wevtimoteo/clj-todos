9278545183dd6f837ae4a3737f3f70f7d6c5004c
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2016-05-28 17:47:51 -0300

    Add Hiccup, JDBC and H2

--- project.clj
+++ project.clj
@@ -4,7 +4,10 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]]
+                 [ring/ring-defaults "0.1.5"]
+                 [hiccup "1.0.2"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [com.h2database/h2 "1.3.170"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler clj-todos.handler/app}
   :profiles
